c0d57d51ecd59a640493931f1d92ea1552751081
aloires <aloires@tutamail.com>
2026-07-07 15:47:46 +0800

    chore: fix bashrc and profile; save shot to XDG_PICTURES_HOME; save rec to XDG_VIDEOS_HOME; update neovim config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,17 @@
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.wrap = false
+vim.o.termguicolors = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.wrap = false
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -6,24 +6,17 @@ Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 Plug 'nvim-treesitter/nvim-treesitter-context'
 Plug 'lukas-reineke/indent-blankline.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
-Plug('akinsho/bufferline.nvim', {['tag'] = '*'})
 
 Plug 'catgoose/nvim-colorizer.lua'
 
-Plug 'nvim-tree/nvim-tree.lua'
-Plug 'nvim-tree/nvim-web-devicons'
-
 Plug('neoclide/coc.nvim', { ['branch'] = 'release' })
 
-Plug 'nvim-mini/mini.nvim'
 Plug 'MeanderingProgrammer/render-markdown.nvim'
 
-Plug 'lilydjwg/fcitx.vim'
+Plug('lewis6991/gitsigns.nvim')
 
 vim.call('plug#end')
 
 require('plugins.indent-blankline')
 require('plugins.colorizer')
-require('plugins.bufferline')
-require('plugins.nvim-tree')
 require('plugins.render-markdown')
